7d0bc8ae172c1d73262f349c8d87e1efb6e5ac6d
sthistl <105175414+sthistl@users.noreply.github.com>
2023-03-04 10:58:59 -0700

    updated patients list file

--- src/patients_list.py
+++ src/patients_list.py
@@ -21,20 +21,27 @@ from flet import (
 
 
 class PatientsList(UserControl):
-    def __init__(self, app,patitents_list, *args, **kwargs):
+    def __init__(self, app, patients_list, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.app = app
-        self.patitents_list = patitents_list
+        self.patients_list = patients_list
         self.build()
-    
+
     def build(self):
         rows = []
-        for patient in self.patitents_list:
+        for patient in self.patients_list:
             rows.append(Row(
                 [
-                    Text(value=patient.name),
+                    Text(value=patient.Fname),
+                    Text(value=patient.Lname),
                     Text(value=patient.age),
+                    Text(value=patient.TOA),
+                    Text(value=patient.CTAS),
+                    Text(value=patient.complaint),
                     Text(value=patient.address),
+                    Text(value=patient.location),
+                    Text(value=patient.rank),
+                    Text(value=patient.record),
                 ],
                 padding=padding(10, 0),
                 horizontal_alignment="center",
@@ -42,9 +49,16 @@ class PatientsList(UserControl):
             ))
         headers = Row(
             [
-                Text(value="Name", style="headlineSmall"),
+                Text(value="Fname", style="headlineSmall"),
+                Text(value="Lname", style="headlineSmall"),
                 Text(value="Age", style="headlineSmall"),
+                Text(value="Arrival Time", style="headlineSmall"),
+                Text(value="CTAS", style="headlineSmall"),
+                Text(value="complaint", style="headlineSmall"),
                 Text(value="Address", style="headlineSmall"),
+                Text(value="location", style="headlineSmall"),
+                Text(value="rank", style="headlineSmall"),
+                Text(value="record", style="headlineSmall"),
             ],
             padding=padding(10, 0),
             horizontal_alignment="center",

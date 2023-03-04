835ebe60ec1c1765913de30d550e6bbddb23157b
sthistl <105175414+sthistl@users.noreply.github.com>
2023-03-04 12:52:49 -0700

    updated users file

--- src/users.py
+++ src/users.py
@@ -1,35 +1,33 @@
 
 import csv
+import datetime
 
 PATIENT_PATH = 'data/patients.csv'
 RECORD_PATH = 'data/records.csv'
 
+
 class Record:
-    def __init__(self, record_id, patient_id, date, time,hart_rate, blood_pressure, temperature, oxygen_saturation, respiratory_rate):
+    def __init__(self, record_id, patient_id, date, time, heart_rate, blood_pressure, temperature, oxygen_saturation, respiratory_rate):
         self.record_id = record_id
         self.patient_id = patient_id
-        self.date = date
-        self.time = time
-        self.hart_rate = hart_rate
+        date = datetime.date(date)
+        time = datetime.time(time)
+        self.date_time = datetime.datetime.combine(date, time)
+        self.heart_rate = heart_rate
         self.blood_pressure = blood_pressure
         self.temperature = temperature
         self.oxygen_saturation = oxygen_saturation
         self.respiratory_rate = respiratory_rate
-
-      
-
-
+        # supplemental oxygen needed later
 
 
 class Patient:
-    def __init__(self,name,age,address,patient_id):
+    def __init__(self, name, age, address, phone_number, patient_id):
         self.name = name
         self.age = age
         self.address = address
         self.patient_id = patient_id
-        self.records = []
-
-
+        self.current_record: Record = None
 
 
 class PatientList:
@@ -38,46 +36,56 @@ class PatientList:
         self.records = []
         self.load_data()
 
-    def get_patients(self):
-        return self.patients
-
-
+    # just most recent one
     def load_data(self):
-        with open(PATIENT_PATH, 'r') as file:
-            reader = csv.reader(file)
-            for row in reader:
-                patient = Patient(row[0], row[1], row[2], row[3], row[4])
-                self.patients.append(patient)
+
         with open(RECORD_PATH, 'r') as file:
             reader = csv.reader(file)
             for row in reader:
-                record = Record(row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8])
+                record = Record(row[0], row[1], row[2], row[3],
+                                row[4], row[5], row[6], row[7], row[8])
                 self.records.append(record)
                 for patient in self.patients:
                     if patient.patient_id == record.patient_id:
                         patient.records.append(record)
+
+        with open(PATIENT_PATH, 'r') as file:
+            reader = csv.reader(file)
+            for row in reader:
+                patient = Patient(row[0], row[1], row[2], row[3], row[4])
+                # need to get 'current record' for patient from get_record
+                latest_record = self.get_record(patient.patient_id)
+                patient.current_record = latest_record
+                # patient append after latest record found
+                self.patients.append(patient)
 
     def get_patient(self, patient_id):
         for patient in self.patients:
             if patient.patient_id == patient_id:
                 return patient
 
-    def get_record(self, record_id):
+    def get_record(self, patient_id):
         for record in self.records:
-            if record.record_id == record_id:
-                return record
+            patient_records = []
+            if record.patient_id == patient_id:
+                patient_records.append(record)
+        if not patient_records:
+            return None
+        return max(patient_records, key=lambda r: r.date_time)
 
     def add_patient(self, patient):
         self.patients.append(patient)
         with open(RECORD_PATH, 'a') as file:
             writer = csv.writer(file)
-            writer.writerow([patient.name, patient.age, patient.address, patient.phone_number, patient.patient_id])
+            writer.writerow([patient.name, patient.age, patient.address,
+                            patient.phone_number, patient.patient_id])
 
     def add_record(self, record):
         self.records.append(record)
         with open(RECORD_PATH, 'a') as file:
             writer = csv.writer(file)
-            writer.writerow([record.record_id, record.patient_id, record.date, record.time, record.hart_rate, record.blood_pressure, record.temperature, record.oxygen_saturation, record.respiratory_rate])
+            writer.writerow([record.record_id, record.patient_id, record.date, record.time, record.hart_rate,
+                            record.blood_pressure, record.temperature, record.oxygen_saturation, record.respiratory_rate])
 
     def update_patient(self, patient):
         for i in range(len(self.patients)):
@@ -87,6 +95,10 @@ class PatientList:
         with open(PATIENT_PATH, 'w') as file:
             writer = csv.writer(file)
             for patient in self.patients:
-                writer.writerow([patient.name, patient.age, patient.address, patient.phone_number, patient.patient_id])
+                writer.writerow([patient.name, patient.age, patient.address,
+                                patient.phone_number, patient.patient_id])
 
-    
+    def update_record(self, record):
+        for i in range(len(self.records)):
+            if self.records[i].record_id == record.record_id:
+                self
